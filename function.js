7f4c55ee6c79affa59256be11c725ce2e75c554e
AarghCreate <67373626+AarghCreate@users.noreply.github.com>
2020-08-23 23:26:16 +0530

    Add files via upload
    Press up arrow key(while it is running) and see what happens

---
+++ function.js
@@ -0,0 +1,16 @@
+function DISPLAY()
+{
+    Top.display("red");
+    Bob1.display("blue");
+    Bob2.display("blue");
+    Bob3.display("blue");
+    Bob4.display("blue");
+    Bob5.display("blue");
+    /*thread1.display();
+    thread2.display();
+    thread3.display();
+    thread4.display();
+    thread5.display();
+    */
+
+}
